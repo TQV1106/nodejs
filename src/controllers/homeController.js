5c2a554b7b146cfe6502fe591462dde76ee5520a
viet11062001 <viet11062001@gmail.com>
2023-03-27 17:33:53 +0700

    create nodejs

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,14 +1,10 @@
 import db from '../models/index';
-
+import CRUDService from '../services/CRUDService';
 
 let getHomePage = async (req,res) => {
 
     try {
         let data = await db.User.findAll();
-        console.log('------------------')
-        console.log(data)
-        console.log('------------------')
-
         return res.render('homepage.ejs', {
             data: JSON.stringify(data)
         });
@@ -22,8 +18,20 @@ let getAboutPage = (req,res) => {
     return res.render('test/about.ejs');
 }
 
+let getCRUD = (req, res) => {
+    return res.render("crud.ejs");
+}
+
+let postCRUD = async (req,res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud from server');
+}
+
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD
 
 }
