51983274d901500ec7cf44b1825ba05e9722f2b4
viet11062001 <viet11062001@gmail.com>
2023-03-31 15:34:05 +0700

    API

--- src/server.js
+++ src/server.js
@@ -3,9 +3,12 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine.js";
 import initWebRoutes from "./route/web.js";
 import connectDB from "./config/connectDB.js";
+import cors from 'cors';
+
 require('dotenv').config();
 
 let app = express();
+app.use(cors({origin: true}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
