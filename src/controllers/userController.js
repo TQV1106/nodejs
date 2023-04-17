194092c01f6e82302c8747e8f46062e2170cb977
viet11062001 <viet11062001@gmail.com>
2023-04-17 15:19:34 +0700

    user-login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -63,6 +63,18 @@ let handleDeleteUser = async (req, res) => {
     let message = await userService.deleteUser(req.body.id);
     return res.status(200).json(message);
 }
+let getAllcode = async (req, res) => {
+    try {
+        let data = await userService.getAllCodeService(req.query.type);
+        return res.status(200).json(data);
+    } catch (error) {
+        console.log('get all code error: ', e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
 
 
 module.exports = {
@@ -70,5 +82,6 @@ module.exports = {
     handleGetAllUsers: handleGetAllUsers,
     handleCreateNewUser: handleCreateNewUser,
     handleEditUser: handleEditUser,
-    handleDeleteUser: handleDeleteUser
+    handleDeleteUser: handleDeleteUser,
+    getAllcode: getAllcode,
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -22,7 +22,7 @@ let handleUserLogin = (email, password) => {
             let isExit = await checkUserEmail(email);
             if(isExit) {
                 let user = await db.User.findOne({
-                    attributes: ['email', 'roleId', 'password'],
+                    attributes: ['email', 'roleId', 'password', 'firstName', 'lastName'],
                     where: {email: email},
                     raw: true
                     
@@ -209,11 +209,36 @@ let updateUserData = (data) => {
     })
 
 }
+
+let getAllCodeService = (typeInput) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            if(!typeInput) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameter'
+                })
+            }else {
+                let res = {};
+                let allcode = await db.Allcode.findAll({
+                    where: {type : typeInput}
+                });
+                res.errCode = 0;
+                res = allcode;
+                resolve(res)
+            }
+           
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
 module.exports = {
     handleUserLogin: handleUserLogin,
     getAllUsers: getAllUsers,
     createNewUser: createNewUser,
     deleteUser: deleteUser,
-    updateUserData: updateUserData
+    updateUserData: updateUserData,
+    getAllCodeService: getAllCodeService
     
 }
