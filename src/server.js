4a2da38f4bd66b1ddb1e287ffe600eab8609e3c0
viet11062001 <viet11062001@gmail.com>
2023-04-20 13:52:39 +0700

    upload img base64

--- src/server.js
+++ src/server.js
@@ -5,33 +5,39 @@ import initWebRoutes from "./route/web.js";
 import connectDB from "./config/connectDB.js";
 // import cors from 'cors';
 
-require('dotenv').config();
+require("dotenv").config();
 
 let app = express();
 // app.use(cors({origin: true}));
 
 app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", process.env.URL_REACT);
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.URL_REACT);
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+  // Pass to next layer of middleware
+  next();
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
@@ -40,5 +46,5 @@ connectDB();
 
 let port = process.env.PORT || 6969;
 app.listen(port, () => {
-    console.log("backend nodejs is running on the port : " + port);
-})
+  console.log("backend nodejs is running on the port : " + port);
+});
